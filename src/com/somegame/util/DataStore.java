d01b55859ab570eff1ff4039d347b7ed83f2bcf9
hirayui1 <playerhasbeentoastedby@gmail.com>
2025-03-30 17:29:04 +0200

    removed import & added TODO

--- src/com/somegame/util/DataStore.java
+++ src/com/somegame/util/DataStore.java
@@ -2,13 +2,9 @@ package com.somegame.util;
 
 import com.somegame.core.location.City;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class DataStore {
 
-    public static City createCityList() {
+    public static City createCityList() { // TODO: later get these from a json or something alike? I've already built a json parser, maybe use that
         City starter = new City("Starter City");
         starter.setNext(new City("Second City"));
         starter.getNext().setNext(new City("Third City"));
